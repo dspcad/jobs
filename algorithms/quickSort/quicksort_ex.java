365d430fc4ff3e51ae7e9af8f19ec494d9bda86e
DSPCAD <virtuoso.wu@gmail.com>
2018-01-29 23:13:55 -0600

    follows CLRS way

--- algorithms/quickSort/quicksort_ex.java
+++ algorithms/quickSort/quicksort_ex.java
@@ -2,32 +2,47 @@
 public class quicksort_ex{
 
   public static void main(String[] args){
-    int [] input_data = {2,3,7,8,1,4,6,9,5};
+    int [] input_data1 = {3,2,1};
+    int [] input_data2 = {1,2,3};
+    int [] input_data3 = {9,8,7,6,5,4,3,2,1,0};
+    //int [] input_data3 = {2,3,7,8,1,4,6,9,5};
 
-    for(int i=0; i<input_data.length;i++)
-      System.out.format("%d ", input_data[i]);
+    //for(int i=0; i<input_data.length;i++)
+    //  System.out.format("%d ", input_data[i]);
+
+    //System.out.println("size: "+input_data.length);
+    quicksort(input_data1, 0, input_data1.length-1);
+    for(int i=0; i<input_data1.length;i++)
+      System.out.format("%d ", input_data1[i]);
 
     System.out.println("");
-    quicksort(input_data, 0, input_data.length-1);
-    for(int i=0; i<input_data.length;i++)
-      System.out.format("%d ", input_data[i]);
+
+    quicksort(input_data2, 0, input_data2.length-1);
+    for(int i=0; i<input_data2.length;i++)
+      System.out.format("%d ", input_data2[i]);
+
+    System.out.println("");
+
+    quicksort(input_data3, 0, input_data3.length-1);
+    for(int i=0; i<input_data3.length;i++)
+      System.out.format("%d ", input_data3[i]);
+
+    System.out.println("");
   }
 
   public static void quicksort(int [] target_array, int start, int end){
-    int i = start;
-    int j = end-1;
-    int pivot = target_array[end];
-
-
     if(start < end){
-      while(i<j){
-        if(target_array[i] <= pivot){
+      int i = start-1;
+      int j = start;
+      int pivot = target_array[end];
+
+
+      while(j<end){
+        if(target_array[j] <= pivot){
           i++;
-        }
-        else{
           swap(target_array,i,j);
-          j--;
         }
+        j++;
       }
 
       swap(target_array,i+1,end);
