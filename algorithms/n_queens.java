c062c1de70f1e82ffcaf252b3039bc852807170b
dspcad <virtuoso.wu@gmail.com>
2018-04-06 17:12:07 -0500

    fixed bugs in else statement forn queens

--- algorithms/n_queens.java
+++ algorithms/n_queens.java
@@ -12,10 +12,6 @@ public class n_queens{
     }
 
     private static void checkValid(int [] col, int row, int n){
-        //int [] col = new int[n];
-        //for(int i=0;i<n;i++)
-        //    col[i] = col_org[i];
-
         if(row == 0){
             for(int i=0;i<n;i++){
                 col[row] = i;
@@ -24,8 +20,6 @@ public class n_queens{
         }
         else if(row == n){
             boolean valid_solution = true;
-            //for(int i=0;i<n;i++)
-            //    System.out.println("col["+i+"]: "+ col[i]);
 
             for(int i=0;i<n;i++)
                 if(col[i] == -1){
@@ -48,28 +42,29 @@ public class n_queens{
         } 
         else{
             for(int i=0;i<n;i++){
+                boolean valid = true;
                 for(int j=0;j<row;j++){
-                    boolean valid = true;
-                    if(col[j]==i)
+                    if(col[j]==i){
                         valid = false;
-
-                    if(Math.abs(row-j) == Math.abs(i-col[j]))
-                        valid = false;
-
-                    System.out.println("row: "+row);
-                    System.out.println("i: "+i);
-                    System.out.println("j: "+j);
-                    System.out.println("col[]: "+col[j]);
-                    if(valid){
-                        col[row] = i;
-                        checkValid(col, row+1, n);
+                        break;
                     }
-                    else
-                        col[row] = -1;
+
+                    if(Math.abs(row-j) == Math.abs(i-col[j])){
+                        valid = false;
+                        break;
+                    }
+
                         
 
                 }
+                if(valid){
+                    col[row] = i;
+                    checkValid(col, row+1, n);
+                }
+                else
+                    col[row] = -1;
             }
+
         }
     }
 }
