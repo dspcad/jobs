e152fcb6a905d62a788be152d63b420392140bfc
Hsiang-Huang Wu <virtuoso.wu@gmail.com>
2018-06-08 00:24:20 -0500

    word search II finished

--- leetcode/word_search_II.java
+++ leetcode/word_search_II.java
@@ -1,15 +1,21 @@
 import java.util.HashMap;
-import java.util.ArrayList;
+import java.util.HashSet;
 
 public class word_search_II{
     public static void main(String args[]){
-        String [] words = {"oath","pea","eat","rain","easy"};
-        char [][] board = {{'o','a','a','n'},
-                           {'e','t','a','e'},
-                           {'i','h','k','r'},
-                           {'i','f','l','v'}};
+        //String [] words = {"oath","pea","eat","rain","easy"};
+        //char [][] board = {{'o','a','a','n'},
+        //                   {'e','t','a','e'},
+        //                   {'i','h','k','r'},
+        //                   {'i','f','l','v'}};
 
-        ArrayList<String> result = new ArrayList<String>();
+        String [] words = {"a"};
+        char [][] board = {{'a'}};
+
+        //String [] words = {"aaa"};
+        //char [][] board = {{'a','a'}};
+
+        HashSet<String> result = new HashSet<String>();
         trie t = new trie();
 
         for(int i=0;i<words.length;i++)
@@ -17,10 +23,33 @@ public class word_search_II{
 
         t.printTrie(t.root);
 
-        //for(int i=0;i<board.length;i++)
-        //    for(int j=0;j<board[i].length;j++){
-        //        check(board, i, j, t.root,)
-        //    }
+        for(int i=0;i<board.length;i++)
+            for(int j=0;j<board[i].length;j++)
+                check(board, i, j, t.root, result);
+            
+ 
+        for(String elem : result)
+            System.out.println(elem);           
+    }
+
+    public static void check(char [][] board, int i, int j, trieNode u, HashSet<String> result){
+        char c = board[i][j];
+
+        if(c == '#' || !u.children.containsKey(c)) return;
+
+        u = u.children.get(c);
+        if(u.found_word != null)      
+            result.add(u.found_word);
+        
+
+        board[i][j] = '#';
+        if(i>0) check(board, i-1, j, u, result);
+        if(j>0) check(board, i, j-1, u, result);
+        if(i<board.length-1) check(board, i+1, j, u, result);
+        if(j<board[i].length-1) check(board, i, j+1, u, result);
+        board[i][j] = c;
+
+
     }
 }
 
@@ -73,6 +102,7 @@ class trie{
                 printTrie(u.children.get(c));
             }
         else{
+            System.out.println("=FOUND=");
             System.out.println(u.found_word);
             System.out.println("========");
         }
