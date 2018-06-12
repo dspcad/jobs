9e15e865306a444e6429ddf9d50a58cccd1e0168
dspcad <virtuoso.wu@gmail.com>
2018-06-12 11:28:06 -0500

    DFS with time stamp

--- CCI189/ch4_7.java
+++ CCI189/ch4_7.java
@@ -1,8 +1,14 @@
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.ArrayList;
 
 class ch4_7{
+    public static int time_stamp;
+
     public static void main(String args[]){
+        time_stamp = -1;
         graph g1 = new graph();
 
         graphNode a = new graphNode("a");
@@ -34,6 +40,46 @@ class ch4_7{
         g1.addEdge(d,g);
 
         g1.printGraph();
+
+        for(graphNode v : g1.adjList.keySet()){
+            if(!v.visited)
+                DFS(v, g1);
+        }
+        
+        ArrayList<graphNode> list = new ArrayList<graphNode>();
+        for(graphNode v : g1.adjList.keySet())
+            list.add(v);
+
+        Collections.sort(list, new nodeComparator());
+
+        for(graphNode v : list){
+            System.out.println("Node: "+v.name);
+            System.out.println("  dicover time: "+ v.discover_time);
+            System.out.println("  finish  time: "+ v.finish_time);
+            System.out.println("");
+        }
+    }
+
+    public static void DFS(graphNode u, graph g){
+        if(!u.visited){
+            time_stamp++;
+            u.discover_time = time_stamp;
+            for(graphNode v : g.adjList.get(u))
+                DFS(v, g);
+
+            time_stamp++;
+            u.finish_time = time_stamp;
+            u.visited = true;
+        }
+    }
+
+    
+}
+
+class nodeComparator implements Comparator<graphNode>{
+    @Override
+    public int compare(graphNode n1, graphNode n2){
+        return n2.finish_time - n1.finish_time;
     }
 }
 
@@ -70,12 +116,12 @@ class graphNode{
     public String name;
     public int discover_time;
     public int finish_time;
-    public boolean finish;
+    public boolean visited;
 
     public graphNode(String x){
         this.name = x;
         this.discover_time = 0;
         this.finish_time = 0;
-        this.finish = false;
+        this.visited = false;
     }
 }
