88a952678edd0d1c12ea9187de1e4be30cb8ff50
Hsiang-Huang Wu <virtuoso.wu@gmail.com>
2018-06-22 11:35:39 -0500

    recursive matrix multiplication

---
+++ algorithms/dynamicProgramming/matrix_multiplication.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+
+public class matrix_multiplication{
+    public static int [][] s = new int[6][6];
+
+    public static void main(String args[]){
+        matrix A1 = new matrix(30,35, "A1");
+        matrix A2 = new matrix(35,15, "A2");
+        matrix A3 = new matrix(15,5, "A3");
+        matrix A4 = new matrix(5,10, "A4");
+        matrix A5 = new matrix(10,20, "A5");
+        matrix A6 = new matrix(20,25, "A6");
+
+        ArrayList<matrix> product = new ArrayList<matrix>();
+
+        product.add(A1);
+        product.add(A2);
+        product.add(A3);
+        product.add(A4);
+        product.add(A5);
+        product.add(A6);
+
+        System.out.println("The number of calculations: "+matrixChain(product, 0, 5));
+
+        for(int i=0;i<6;i++){
+            for(int j=0;j<6;j++)
+                System.out.printf("%d ", s[i][j]);
+
+            System.out.println("");
+        }
+          
+        printSol(product, s, 0, 5);
+        System.out.println("");
+    }
+
+    public static void printSol(ArrayList<matrix> list, int [][] s, int i, int j){
+        if(i==j)
+            System.out.printf("%s", list.get(i).name);
+        else{
+            System.out.printf("(");
+            printSol(list, s, i, s[i][j]);
+            printSol(list, s, s[i][j]+1, j);
+            System.out.printf(")");
+        }
+    }
+
+    public static int matrixChain(ArrayList<matrix> list, int i, int j){
+        if(i >= j)
+            return 0;
+
+        int min = Integer.MAX_VALUE;
+        for(int k=i;k<j;k++){
+            int cost = matrixChain(list, i,k)+matrixChain(list, k+1,j) + list.get(i).r*list.get(k).c*list.get(j).c;
+            if(cost < min){
+                min = cost;
+                s[i][j] = k;
+            }
+        }
+
+
+        return min;
+    }
+}
+
+
+class matrix{
+    public int r;
+    public int c;
+    public String name;
+
+    public matrix(int x, int y, String str){
+        this.r = x;
+        this.c = y;
+        this.name = str;
+    }
+}
