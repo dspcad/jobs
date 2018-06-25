4a9a935a1c86f6949b8a643a7f6d0127d7c0d1a9
dspcad <virtuoso.wu@gmail.com>
2018-06-25 16:33:59 -0500

    remove duplicate using hashSet<String>

--- leetcode/three_sum.java
+++ leetcode/three_sum.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 
 public class three_sum{
     public static void main(String args[]){
-        int [] nums = {-1, 0, 1, 2, -1, -4};
+        int [] nums = {0, 0, 0};
+        //int [] nums = {-1, 0, 1, 2, -1, -4};
         Arrays.sort(nums);
 
         ArrayList<ArrayList<Integer>> result = sum(nums);
@@ -19,6 +20,8 @@ public class three_sum{
         HashMap<Integer,HashSet<Integer>> tbl = new HashMap<Integer,HashSet<Integer>>();
         ArrayList<ArrayList<Integer>> output = new ArrayList<ArrayList<Integer>>();
         HashSet<Integer> indexSet;
+
+        HashSet<String> solSet = new HashSet<String>();
 
         for(int i=0;i<nums.length;i++){
             if(tbl.containsKey(nums[i])){
@@ -36,11 +39,14 @@ public class three_sum{
         for(Integer elem : tbl.keySet())
             System.out.println("key: "+elem+"  value: "+tbl.get(elem));
 
-
+        boolean zeros = false;
 
         for(int i=0;i<nums.length-2;i++){
             int a = nums[i];
             
+            if(a > 0)
+                break;
+
             for(int j=i+1;j<nums.length;j++){
                 int b = nums[j];
 
@@ -52,20 +58,39 @@ public class three_sum{
                             c_flag = true;
 
 
-                    if(c_flag){
+                    if(c_flag && !checkDuplicate(solSet, a, b, -a-b)){
                         ArrayList<Integer> sol = new ArrayList<Integer>();
                         sol.add(a);
                         sol.add(b);
                         sol.add(-a-b);
                         output.add(sol);
-
+                        
+                        if(a==0 && b==0){
+                            zeros = true;
+                            break;
+                        }
                     }
                 }
             }
 
+            if(zeros)
+                break;
         }
 
         return output;
     }
 
+    public static boolean checkDuplicate(HashSet<String> solSet, int a, int b, int c){
+        StringBuilder str = new StringBuilder(3);
+        str.append(a);
+        str.append(b);
+        str.append(c);
+
+        if(solSet.contains(str.toString()))
+            return true;
+        else{
+            solSet.add(str.toString());
+            return false;
+        }
+    }
 }
